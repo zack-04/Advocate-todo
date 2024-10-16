f6a932d1eac77caeab695ea09636ea2a18b0da26
Ripunjay Das <rjdas555@gmail.com>
2024-10-16 11:09:54 +0530

    Add login feature

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+String encKey = 'iq8xkfInuzVYYnE4YIpapvQUg6uU';
+
+class ApiConstants {
+  static const String baseUrl = 'https://todo.sortbe.com/api/';
+  static const String loginEndpoint = '${baseUrl}Login/Login';
+}
